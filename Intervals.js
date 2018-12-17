bc5995e38e8e4a1b53338c0454b1813d76a4332b
TylwythTeg <garciarobertoiv@yahoo.com>
2018-12-16 23:45:24 -0800

    First Commit

---
+++ Intervals.js
@@ -0,0 +1,99 @@
+const Utils = require('./Utils.js');
+
+const noteRegExp = "[A-G]";
+const intervalDigitRegExp = "[1,2,3,4,5,6,7,8,9,10,11,12,13]+";
+const accidentalRegExp = "[#,b]+";
+
+const intervals = [];
+
+function Interval(name) {
+    this.name = name;
+    intervals.push(this);
+    this.value = intervals.indexOf(this);
+}
+
+Interval.prototype.flat = function() {
+    var value = (this.value - 1) % 12;
+    return intervals[value];
+};
+
+Interval.prototype.sharp = function() {
+    var value = (this.value + 1) % 12;
+    return intervals[value];
+};
+
+var names = [
+    "1",
+    "b2",
+    "2",
+    "b3",
+    "3",
+    "4",
+    "#4",
+    "5",
+    "b6",
+    "6",
+    "b7",
+    "7"
+];
+
+(function buildNotes() {
+    names.forEach(function(str) {
+        Interval[str] = new Interval(str);
+    });
+})();
+
+/* Export */
+var Intervals = {};
+Intervals.list = intervals;
+
+Intervals.fromName = function(name) {
+    return Utils.Array.getFrom("name", name, intervals);
+};
+
+Intervals.fromValue = function(value) {
+    return Utils.Array.getFrom("value", value, intervals);
+};
+
+
+Intervals.getAccidentalValue = function(accidentalString) {
+    var count = 0;
+    for (i in accidentalString) {
+        var char = accidentalString[i];
+
+        if (char === "#") {
+            count++;
+        } else if (char === "b") {
+            count--;
+        }
+    }
+    return count;
+}
+
+
+
+Interval.value = function(intervalString) {
+    var referenceInterval = intervalString.match(intervalDigitRegExp)[0];
+    var referenceIntervalObject = Intervals.fromName(referenceInterval);
+    var referenceValue = referenceIntervalObject.value;
+
+    var accidental = intervalString.match(accidentalRegExp)[0];
+    var accidentalValue = Intervals.getAccidentalValue(accidental);
+
+    if (instruction === 0) {
+        return referenceValue;
+    }
+
+    var instruction = accidentalValue < 0 ? "flat" : "sharp";
+
+    var count = Math.abs(accidentalValue);
+    var newInterval = null;
+    for (var i = 0; i < count; i++) {
+        newInterval = referenceIntervalObject[instruction]();
+    }
+    return newInterval.value;
+};
+
+
+
+module.exports = Intervals;
---
+++ Notes.js
@@ -0,0 +1,93 @@
+const Interval = require('./Intervals.js');
+const Utils = require('./Utils.js');
+
+const noteRegExp = "[A-G]";
+const accidentalRegExp = "[#,b]+";
+
+
+const notes = [];
+
+/* Note */
+function Note(name) {
+    this.name = name;
+    notes.push(this);
+    this.value = notes.indexOf(this);
+}
+
+Note.prototype.flat = function() {
+    const isFirstNote = (this.value === 0);
+    const getLast = (notes.length - 1);
+    const defaultCase = (this.value - 1) % 12;
+
+    const value = isFirstNote ? getLast : defaultCase;
+    return notes[value];
+  };
+
+  Note.prototype.sharp = function() {
+    const isLastNote = (this.value === (notes.length -1));
+    const getFirst = 0;
+    const defaultCase = ((this.value - 1) % 12);
+
+    const value = isLastNote ? getFirst : defaultCase;
+    return notes[value];
+  };
+
+var names = [
+    "A",
+    "A#",
+    "B",
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#"
+];
+
+(function buildNotes() {
+    names.forEach(function(str) {
+        Note[str] = new Note(str);
+    });
+})();
+
+/* Export */
+var Notes = {};
+Notes.list = notes;
+
+
+
+Notes.fromName = function(name) {
+    var note = fromCustomName(name);
+    note = Object.create(note);
+    return note;
+};
+
+Notes.fromValue = function(value) {
+    return Utils.Array.getFrom("value", value, notes);
+};
+
+/* Accepts [A-G] + (b|#)? */
+function fromNormalizedName(name) {
+  return Utils.Array.getFrom("name", name, notes);
+}
+
+/* Accepts [A-G] + any sequence of #s or bs */
+function fromCustomName(str) {
+  const referenceNoteString = str.match(noteRegExp)[0];
+  const referenceNote = fromNormalizedName(referenceNoteString);
+  const accidentalString = str.match(accidentalRegExp)[0];
+  const accidentalValue = Interval.getAccidentalValue(accidentalString);
+
+  var index = (referenceNote.value + accidentalValue) % 12;
+  return notes[index];
+}
+
+module.exports = Notes;
+
+// Test space
+
+console.log(Notes.list);
+console.log(Notes.fromName("G###").name);
